b757552a6c615976c764965c82d7925366aca9d5
baastollie baas <baastollie121@gmail.com>
2025-07-14 06:54:34 +0000

    Changes before Firebase Studio auto-run

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'rak-safety-hub-n0boorak-safety-hub-n0boo',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'rak-safety-hub-n0boorak-safety-hub-n0boo',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
